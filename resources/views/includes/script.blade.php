9a267bdd52e88d67d51ab84cb0418c5cdd42b725
Wikukarnoprasetyagama <prasetyagama2@gmail.com>
2022-03-20 17:49:18 +0700

    update file script & style home

--- resources/views/includes/script.blade.php
+++ resources/views/includes/script.blade.php
@@ -1,7 +1,7 @@
 <!-- Script -->
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js" integrity="sha512-894YE6QWD5I59HgZOGReFYm4dnWc1Qt5NtvYSaNcOP+u1T9qYdvdihz0PPSiiqn/+/3e7Jo4EaG7TubfWGUrMQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
-<script src="{{ asset('/frontend/libraries/bootstrap/js/bootstrap.min.js') }}"></script>
-<script src="{{ asset('/frontend/libraries/aos/aos.js') }}"></script>
+<script src="{{ asset('/public/frontend/libraries/bootstrap/js/bootstrap.min.js') }}"></script>
+<script src="{{ asset('/public/frontend/libraries/aos/aos.js') }}"></script>
 <script>
   AOS.init();
 </script>
