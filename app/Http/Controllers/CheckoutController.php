2d7c1abc0724648726beac3df060716960b5b3d8
Wikukarnoprasetyagama <prasetyagama2@gmail.com>
2022-04-16 21:49:43 +0700

    update file checkout

--- app/Http/Controllers/CheckoutController.php
+++ app/Http/Controllers/CheckoutController.php
@@ -164,6 +164,6 @@ class CheckoutController extends Controller
         }
 
         $transaction->save();
-        return redirect('success');
+        return redirect()->view('success');
     }
 }
