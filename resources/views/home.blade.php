ffece682d3d013c9cba9393f7f2157c203301306
Wikukarnoprasetyagama <prasetyagama2@gmail.com>
2022-05-25 17:24:12 +0700

    update kode google login

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -154,3 +154,26 @@
     	</div>
     </section>
 @endsection
+
+@push('after-script')
+    {{-- // Google Screen Login --}}
+<script src="https://accounts.google.com/gsi/client"></script>
+<script>
+  const client = google.accounts.oauth2.initCodeClient({
+  client_id: '{{ config('services.google.client_id') }}',
+  scope: 'https://www.googleapis.com/auth/calendar.readonly',
+  ux_mode: 'popup',
+  callback: (response) => {
+    const xhr = new XMLHttpRequest();
+    xhr.open('POST', code_receiver_uri, true);
+    xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+    // Set custom header for CRSF
+    xhr.setRequestHeader('X-Requested-With', 'XmlHttpRequest');
+    xhr.onload = function() {
+      console.log('Auth code response: ' + xhr.responseText);
+    };
+    xhr.send('code=' + code);
+  },
+});
+</script>
+@endpush
