bca3de6c599dfa955c95f23e09d3e26a7bfd2864
Wikukarnoprasetyagama <prasetyagama2@gmail.com>
2022-05-29 17:26:26 +0700

    update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -70,12 +70,13 @@
         </div>
     </div>
   </div>
+  
   @include('includes.admin.script')
+  
+  @include('sweetalert::alert')
+
   @stack('before-script')
   <script src="https://cdn.datatables.net/1.11.3/js/jquery.dataTables.min.js"></script>
   @stack('after-script')
-  {{-- <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js" integrity="sha512-894YE6QWD5I59HgZOGReFYm4dnWc1Qt5NtvYSaNcOP+u1T9qYdvdihz0PPSiiqn/+/3e7Jo4EaG7TubfWGUrMQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script> --}}
-  {{-- @include('sweetalert::alert', ['cdn' => "https://cdn.jsdelivr.net/npm/sweetalert2@9"]) --}}
-  @include('sweetalert::alert')
 </body>
 </html>
