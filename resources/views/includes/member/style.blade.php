b3124affdc9daac3317b5d77e27c3eb38d810958
Wikukarnoprasetyagama <prasetyagama2@gmail.com>
2022-06-09 21:20:45 +0700

    update fontawesome

--- resources/views/includes/member/style.blade.php
+++ resources/views/includes/member/style.blade.php
@@ -1,6 +1,8 @@
   <!-- General CSS Files -->
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" integrity="sha512-iBBXm8fW90+nuLcSKlbmrPcLa0OT92xO1BIsZ+ywDWZCvqsWgccV3gFoRBv0z+8dLJgyAHIhR35VZc2oM/gI1w==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" integrity="sha512-1ycn6IcaQQ40/MKBW2W4Rhis/DbILU74C1vSrLJxCq57o941Ym01SwNsOMqvEBFlcgUa6xLiPY/NS5R+E6ztJQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+  
   <!-- CSS Libraries -->
   <link rel="stylesheet" href="{{ url('/jqvmap/dist/jqvmap.min.css') }}">
   <link rel="stylesheet" href="{{ url('/summernote/dist/summernote-bs4.css') }}">
