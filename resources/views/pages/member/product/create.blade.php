d10727da030f2bada32862258a873aab469e2732
Wikukarnoprasetyagama <prasetyagama2@gmail.com>
2022-06-09 15:10:20 +0700

    add ckeditor

--- resources/views/pages/member/product/create.blade.php
+++ resources/views/pages/member/product/create.blade.php
@@ -111,7 +111,7 @@
                                 <div class="form-group">
                                     <label for="description" class="form-control-label">Isi Deskripsi Produk</label>
                                     <textarea name="description" style="height: 70px"
-                                            class="form-control @error('description') is-invalid @enderror">{{ old('description') }}</textarea>
+                                            class="ckeditor form-control @error('description') is-invalid @enderror">{{ old('description') }}</textarea>
                                     @error('description') <div class="text-muted">{{ $message }}</div>@enderror
                                 </div>
                             </div>
